1d7c6bead352c18306e519bacea15a4559b34f43
Fernanda <fecoimbra96@gmail.com>
2025-09-28 01:24:19 -0300

    feat: componente planta

---
+++ src/components/Planta.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Planta() {
+  return (
+    <div className="card planta">
+      <h2>Planta</h2>
+      {/* <img className="plant-image"
+        alt="Planta"
+      /> */}
+    </div>
+  );
+}
+
+export default Planta;
